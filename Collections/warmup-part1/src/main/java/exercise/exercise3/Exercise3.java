a6d91018bb3452d988a396f01417d70f70d933e5
andreea.dima <dimaandreea96@yahoo.com>
2017-07-05 17:35:59 +0300

    Collections

---
+++ Collections/warmup-part1/src/main/java/exercise/exercise3/Exercise3.java
@@ -0,0 +1,64 @@
+package exercise.exercise3;
+
+import java.util.*;
+
+/**
+ * Created by Radu.Hoaghe on 04/20/2015.
+ *
+ * Exercise 3: Fill three Set implementations that you know (Hint: they were described during
+ *             the earlier presentation) with the List<String> that is given to this class by
+ *             its constructor.
+ *
+ *             Check out the elements that the list mentioned above contains and then, add them
+ *             to your three Sets. After this check out the elements of your Sets. What do you
+ *             remark? What could be the reason?
+ *
+ *             Finally, add to the one of the three Sets some elements
+ *             that already exist in the Set (e.g add("that") and add("collection"))
+ *
+ *             To run your implementation, run the Exercise3Test class.
+ */
+public class Exercise3 {
+
+    // List containing some elements that need to be added into the Set
+    private List<String> listToAdd;
+
+    public Exercise3(List<String> l) {
+        listToAdd = l;
+    }
+
+    public void addElementsToSets(){
+
+        System.out.println("The elements that will be added to the Sets: ");
+        // TODO Exercise #3 a) Check the content of the elements you will add into the Set
+          for(String s: listToAdd)
+              System.out.println(s);
+        // TODO Exercise #3 b) add the elements from listToAdd to the Sets
+        Set<String> s1 = new HashSet<String>();
+        s1.addAll(listToAdd);
+
+        Set<String> s2 = new TreeSet<String>();
+        s2.addAll(listToAdd);
+
+        Set<String> s3 = new LinkedHashSet<String>();
+        s3.addAll(listToAdd);
+
+        // TODO Exercise #3 c) Check the content of the Sets
+        System.out.println("\nThe elements contained in the first Set (HashSet): ");
+        System.out.println(s1);
+
+        System.out.println("\nThe elements contained in the second Set (TreeSet): ");
+        System.out.println(s2);
+        System.out.println("\nThe elements contained in the third Set (LinkedHashSet): ");
+        System.out.println(s3);
+
+        System.out.println("\nThe elements contained in the TreeSet after inserting two duplicates: ");
+
+        // TODO Exercise #3 d) Add to the TreeSet two elements that already exist in the Set
+        // TODO Exercise #3 d) and print again the TreeSet. What do you see?
+        s2.add("duplicate");
+        s2.add("elements");
+
+        System.out.println(s2);
+    }
+}
